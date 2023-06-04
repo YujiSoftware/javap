08cd265d184f782a12bde0541be6a17b05edd47d
YujiSoftware <yuji.software+github@gmail.com>
2023-06-04 11:05:09 +0900

    Add string-switch sample

---
+++ string-switch/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        switch(args[0]) {
+            case "ABC":
+                System.out.println("ABC");
+                break;
+            case "XYZ":
+                System.out.println("XYZ");
+                break;
+        }
+    }
+}
